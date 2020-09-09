21b8fc0a8daa5207ab5ae06dd684c5e642f2dc27
drigos1er <drigos1er@yahoo.fr>
2020-09-09 18:23:51 +0200

    PROJECT UPGRADE SYMFONY 4.4

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,39 +2,39 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityController extends Controller
+class SecurityController extends AbstractController
 {
-    /**
-     * @Route("/login", name="login")
-     */
-    public function loginAction(Request $request)
+
+    public function login(Request $request, AuthenticationUtils $authenticationUtils)
     {
-        $authenticationUtils = $this->get('security.authentication_utils');
 
-        $error = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', array(
-            'last_username' => $lastUsername,
-            'error'         => $error,
-        ));
+
+        return $this->render('security/login.html.twig', [
+            'error' =>$authenticationUtils->getLastAuthenticationError(),
+            'last_username' =>$authenticationUtils->getLastUsername(),
+            'current_menu'=>'login'
+
+        ]);
+
+
+
+
     }
 
-    /**
-     * @Route("/login_check", name="login_check")
-     */
+
     public function loginCheck()
     {
         // This code is never executed.
     }
 
-    /**
-     * @Route("/logout", name="logout")
-     */
+
     public function logoutCheck()
     {
         // This code is never executed.
--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
-use AppBundle\Entity\Task;
-use AppBundle\Form\TaskType;
+
+use App\Entity\Task;
+use App\Form\TaskType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class TaskController extends Controller
+class TaskController extends AbstractController
 {
     /**
      * @Route("/tasks", name="task_list")
