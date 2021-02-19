7d7c944e1c8aa15ac1d694afe186b8754c08401f
drigos1er <drigos1er@yahoo.fr>
2021-02-19 02:07:30 +0100

    CONTRIBUTION 4

--- config/todoadm.sql
+++ config/todoadm.sql
@@ -1,5 +1,11 @@
 INSERT INTO `user` (`id`, `username`, `password`, `email`, `user_role`) VALUES
 (8, 'todoadm', '$2y$13$g2D326TAtfVUj5uEwzHyROF5bn63IxOn5c67CkiDFJznV7D4s5DcK', 'todoadm@todo.ci', '1');
 
+
+INSERT INTO `to_do_role` (`id`, `title`) VALUES
+(1, 'ROLE_ADMIN'),
+(2, 'ROLE_USER');
+
+
 INSERT INTO `to_do_role_user` (`to_do_role_id`, `user_id`) VALUES
 (1, 8)
