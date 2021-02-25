adec1cb81eb4fd1811f18963a86445dc556da9c9
drigos1er <39566567+drigos1er@users.noreply.github.com>
2021-02-25 10:18:44 +0100

    Update AppFixtures.php

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\DataFixtures;
-
+use App\Entity\ToDoRole;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+
 
 class AppFixtures extends Fixture
 {
